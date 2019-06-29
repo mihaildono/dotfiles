00cdc7c93f7de8307b163b0f9eed41200ae1a482
Mihail <mihailgeorgiev94@gmail.com>
2019-06-29 20:01:16 +0300

    Remove nodejs

--- config.sh
+++ config.sh
@@ -11,10 +11,7 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 echo "\nInstalling packages via apt...\n"
 sudo add-apt-repository ppa:kelleyk/emacs -y
 sudo apt update
-sudo apt install git python-virtualenv terminator zsh nodejs gnome-tweak-tool python3-pip emacs26 libssl-dev libreadline-dev zlib1g-dev -y
-
-echo "\nInstalling npm packages...\n"
-sudo npm install -g tern pure-prompt --allow-root --unsafe-perm=true
+sudo apt install git python-virtualenv terminator zsh gnome-tweak-tool python3-pip emacs26 libssl-dev libreadline-dev zlib1g-dev -y
 
 echo "\nInstalling pip packages...\n"
 pip3 install pylint flake8 jedi
@@ -44,9 +41,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/
 echo "\nInstalling rbenv...\n"
 wget -q https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer -O- | bash
 
-echo "\nInstalling NVM...\n"
+echo "\nInstalling NVM and node...\n"
 mkdir ~/.nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+nvm install node
+
+echo "\nInstalling npm packages...\n"
+sudo npm install -g tern pure-prompt --allow-root --unsafe-perm=true
 
 ## for pure-prompt if symlink is not established, use:
 ## cd /usr/lib/node_modules or .nvm/node_ver/node_modules
