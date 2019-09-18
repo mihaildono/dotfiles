3d67c6af2880c07065056bb4af57287c21b57320
Mihail <mihailgeorgiev94@gmail.com>
2019-09-18 14:50:48 +0300

    Add multiple-cursors

--- init.el
+++ init.el
@@ -201,6 +201,13 @@
 
 (use-package diminish)
 
+(use-package multiple-cursors
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
 (use-package haml-mode
   :mode ("\\.haml$" . haml-mode)
 
