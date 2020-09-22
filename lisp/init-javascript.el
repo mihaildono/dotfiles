4781f1e120cdabda0b767e98e871699531c6512f
Mihail Georgiev <mihaildono@gmail.com>
2020-09-22 11:23:00 +0300

    Simplify init-javascript

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -2,32 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(maybe-require-package 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
 
-;;; Basic js-mode setup
-
-(add-to-list 'auto-mode-alist '("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . js-mode))
-
-;; Change some defaults: customize them to override
-(setq-default js2-bounce-indent-p nil)
-(with-eval-after-load 'js2-mode
-  ;; Disable js2 mode's syntax error highlighting by default...
-  (setq-default js2-mode-show-parse-errors nil
-                js2-mode-show-strict-warnings nil)
-  ;; ... but enable it if flycheck can't handle javascript
-  (autoload 'flycheck-get-checker-for-buffer "flycheck")
-  (defun sanityinc/enable-js2-checks-if-flycheck-inactive ()
-    (unless (flycheck-get-checker-for-buffer)
-      (setq-local js2-mode-show-parse-errors t)
-      (setq-local js2-mode-show-strict-warnings t)
-      (when (derived-mode-p 'js-mode)
-        (js2-minor-mode 1))))
-  (add-hook 'js-mode-hook 'sanityinc/enable-js2-checks-if-flycheck-inactive)
-  (add-hook 'js2-mode-hook 'sanityinc/enable-js2-checks-if-flycheck-inactive)
-  (js2-imenu-extras-setup))
+(setq js-indent-level 2)
 
 (when (maybe-require-package 'add-node-modules-path)
-  (dolist (mode '(typescript-mode js-mode js2-mode coffee-mode))
+  (dolist (mode '(js-mode))
     (add-hook (derived-mode-hook-name mode) 'add-node-modules-path)))
 
 (provide 'init-javascript)
