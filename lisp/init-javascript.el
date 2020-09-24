280d766c53cd792bffa09ae82cc0f48d726ffa16
Mihail Georgiev <mihaildono@gmail.com>
2020-09-24 17:30:07 +0300

    Add typescript-mode

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -3,10 +3,12 @@
 ;;; Code:
 
 (maybe-require-package 'js2-mode)
+(maybe-require-package 'typescript-mode)
 
 (add-hook 'js-mode-hook 'js2-minor-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-mode))
 
 (autoload 'sgml-electric-tag-pair-mode
   "sgml-mode" "Auto edits html tags" t)
@@ -23,9 +25,9 @@
 (global-set-key (kbd "C-c /") 'sgml-close-tag)
 
 (setq js2-strict-missing-semi-warning nil)
-
-;; set indent level to 2
 (setq js-indent-level 2)
+(setq typescript-indent-level 2)
+
 
 (when (maybe-require-package 'add-node-modules-path)
   (dolist (mode '(js-mode js2-mode))
