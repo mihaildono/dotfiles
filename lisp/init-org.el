a98e5e18b0751d348a1432615a795c3b2e2317cd
Mihail Georgiev <mihaildono@gmail.com>
2020-10-21 18:24:40 +0300

    Remove pomodoro

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,7 +3,6 @@
 ;;; Code:
 
 ;; Agenda
-
 (define-key global-map (kbd "C-c a") 'org-agenda)
 (setq org-agenda-files '("~/.emacs.d/org/notes/"))
 (setq org-agenda-todo t)
@@ -36,14 +35,7 @@
       org-directory "~/.emacs.d/org")
 
 ;;; Archiving
-
 (setq org-archive-location "%s_archive::* Archive")
-
-;; Pomodoro
-(require-package 'org-pomodoro)
-(setq org-pomodoro-keep-killed-pomodoro-time t)
-(with-eval-after-load 'org-agenda
-  (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro))
 
 (provide 'init-org)
 ;;; init-org.el ends here
