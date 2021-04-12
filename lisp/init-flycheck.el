e18f8a61515c877c653ddcbe9bdf5ba2385eef61
Mihail Georgiev <mihaildono@gmail.com>
2021-04-12 14:45:34 +0300

    Add fix for eslint in web-mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -5,9 +5,15 @@
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
   ;; display error messages in the echo area
-  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  ;;(flycheck-add-javascript 'mode-eslint 'web-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
+(add-hook 'web-mode-hook
+          #'(lambda ()
+              (unless (flycheck-checker-supports-major-mode-p
+                       'javascript-eslint 'web-mode)
+                (flycheck-add-mode 'javascript-eslint 'web-mode))
+              (flycheck-mode t)))
 ;; config file
 ;; (custom-set-variables
 ;;  '(flycheck-typescript-tslint-config "~/tslint.json"))
